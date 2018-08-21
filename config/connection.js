234d5c78d13bca39e59d605dbb7b98321ebf073c
Samuel-Hall <samuel.r.hall14@gmail.com>
2018-08-21 17:48:47 -0400

    jaws

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,17 @@ var connection = mysql.createConnection({
   socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+  })
+}
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
